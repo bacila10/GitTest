43cc24512226548f45b4712cb01fb1a88db11109
bacila10 <bacila10@gmail.com>
2023-02-06 10:28:32 +0200

    pirmas issaugojimas

--- app/src/main/java/com/bandymas/gittest/MainActivity.kt
+++ app/src/main/java/com/bandymas/gittest/MainActivity.kt
@@ -2,8 +2,11 @@ package com.bandymas.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bandymas.gittest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
